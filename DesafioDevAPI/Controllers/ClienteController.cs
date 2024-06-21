7621f4a41043fec8c4e7109b680f5aff94d0619f
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2024-06-20 22:43:01 -0300

    first commit

---
+++ DesafioDevAPI/Controllers/ClienteController.cs
@@ -0,0 +1,17 @@
+﻿using DesafioDevAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioDevAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Cliente>> FindAll()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ DesafioDevAPI/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace DesafioDevAPI.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string None { get; set; }
+        public string Email {  get; set; }
+
+
+
+    }
+}
