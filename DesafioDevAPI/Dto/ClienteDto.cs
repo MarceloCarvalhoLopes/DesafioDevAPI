4be0d777dccc11c755eda7d2349a44515881a8c6
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2024-06-24 08:27:06 -0300

    update ClienteDTO

--- DesafioDevAPI/Dto/ClienteDto.cs
+++ DesafioDevAPI/Dto/ClienteDto.cs
@@ -2,7 +2,6 @@
 {
     public class ClienteDto
     {
-        public int Id { get; set; }
         public string Nome { get; set; }
         public string Cpf { get; set; }
         public string Uf { get; set; }
